a8d7c65acb9fc538578ce34290391be3ff29ad8a
romainbessugesmeusy <romainbessuges@gmail.com>
2017-07-25 11:21:33 +0200

    Fix upload et mandrin

--- src/routes.php
+++ src/routes.php
@@ -31,8 +31,8 @@ $app->post('/v1/quote', function (Application $app, Request $request) use ($smar
         $params->get('quantity'),
         $params->get('height'),
         $params->get('width'),
-        $params->get('application') == 'automatic',
-        $params->get('core_size'),
+        ($params->get('application', 'manual') == 'automatic'),
+        new \Adesa\SmartLabelClient\Mandrin($params->get('core_size')),
         $params->get('nb_labels_per_reel'),
         $params->get('nb_reels'),
         $params->get('orientation'),
@@ -49,26 +49,41 @@ $app->post('/v1/quote', function (Application $app, Request $request) use ($smar
 
 
 $app->post('/v1/order', function (Application $app, Request $request) use ($smartLabel) {
-    $params = $request->request;
+    $filesToDelete = [];
+    $uploadDir = __DIR__ . '/../uploads';
 
+    $params = $request->request;
     $dossier = new \Adesa\SmartLabelClient\Dossier();
     $dossier->numero = $params->get('quote_id');
-    $dossier->quantitesParSerie = $params->get('nb_labels_per_versions');
+    $dossier->scenario = new \Adesa\SmartLabelClient\Scenario($params->get('scenario_id'));
+    $dossier->quantitesParSerie = implode(';', $params->get('nb_labels_per_versions'));
 
     $bdc = $smartLabel->commander($dossier, $params->get('external_order_id'));
 
     $versionsTitles = $params->get('versions_titles');
 
-    foreach ($params->get('versions_files') as $i => $version) {
-        $bdc->ajouterModele($versionsTitles[$i], $params->get("versions_files"));
+    /**
+     * @var $version Symfony\Component\HttpFoundation\File\UploadedFile
+     */
+    foreach ($request->files->get('versions_files') as $i => $version) {
+        $filename = uniqid('upload') . '.' . $version->getClientOriginalExtension();
+        $version->move($uploadDir, $filename);
+        $bdc->ajouterModele($versionsTitles[$i], $uploadDir . '/' . $filename);
+        $filesToDelete[] = $uploadDir . '/' . $filename;
     }
 
     $smartLabel->finaliserBonDeCommande($bdc);
+
+    foreach($filesToDelete as $file){
+        unlink($file);
+    }
 
     return $app->json([
         "order_reference" => $bdc->reference,
         "quote_id" => $bdc->dossier->numero
     ]);
+
+
 });
 
 $app->get('/v1/status', function(Application $app, Request $request) use ($smartLabel) {
